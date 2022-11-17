134373f6610cedf02af7b1261dffc416972977ea
wilson nie <wilson.nie13@gmail.com>
2022-11-17 10:43:22 -0800

    added notes folder

---
+++ src/pages/notes/GithubNotes.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div>Basic Github Notes</div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GithubNotes',
+});
+</script>
