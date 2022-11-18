029978750e12c4a1440519bb8ea866e225a123c2
wilson nie <wilson.nie13@gmail.com>
2022-11-17 22:59:15 -0800

    clickable card

--- src/pages/notes/Notes.vue
+++ src/pages/notes/Notes.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div>notes page</div>
-    <q-card class="my-card bg-white">
+    <q-card v-ripple class="cursor-pointer q-hoverable" bordered square>
       <q-card-section align="center">
         <q-icon name="fa-solid fa-folder" size="xl" />
       </q-card-section>
       <q-separator />
-      <q-card-section> Github Notes </q-card-section>
+      <q-card-section align="center"> Github Notes </q-card-section>
     </q-card>
     <q-icon name="fa-solid fa-folder" size="xl" />
     <i class="fa-solid fa-user"></i>
