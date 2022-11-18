ffdb78fc181e90f071c86a28e9f03bf66cd59114
wilson nie <wilson.nie13@gmail.com>
2022-11-18 03:09:14 -0800

    basic route navigation

--- src/pages/notes/Notes.vue
+++ src/pages/notes/Notes.vue
@@ -1,7 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div>notes page</div>
-    <q-card v-ripple class="card cursor-pointer q-hoverable" bordered square>
+    <q-card
+      v-ripple
+      class="card cursor-pointer q-hoverable"
+      bordered
+      square
+      @click="test"
+    >
       <q-card-section align="center">
         <q-icon name="fa-solid fa-folder" size="xl" />
       </q-card-section>
@@ -15,9 +21,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'GithubNotes',
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+    const test = () => {
+      router.push({ path: '/notes/github-notes' });
+    };
+    return { test };
+  },
 });
 </script>
 
