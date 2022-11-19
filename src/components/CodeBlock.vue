040a9d4061d30fc6b34078aa82d71a18db3b8322
wilson nie <wilson.nie13@gmail.com>
2022-11-18 22:29:34 -0800

    first iteration of code block component

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-card class="code-block">
+    <q-card-section> sdfklsjfkdsfls </q-card-section>
+    <q-icon name="fa-solid fa-clipboard" class="absolute-top-right"></q-icon>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CodeBlock',
+});
+</script>
+
+<style scoped>
+.code-block {
+  width: 80%;
+  background-color: #b0bec5;
+}
+</style>
--- src/pages/notes/DockerNotes.vue
+++ src/pages/notes/DockerNotes.vue
@@ -1,13 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div>Basic Docker Notes</div>
+    <code-block></code-block>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import CodeBlock from 'src/components/CodeBlock.vue';
 export default defineComponent({
   name: 'DockerNotes',
+  components: {
+    CodeBlock,
+  },
 });
 </script>
