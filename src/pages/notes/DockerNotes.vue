1bfa51f9096b98e22f512055b865922306c3ed72
wilson nie <wilson.nie13@gmail.com>
2022-11-19 22:28:49 -0800

    adding docker notes contents

--- src/pages/notes/DockerNotes.vue
+++ src/pages/notes/DockerNotes.vue
@@ -31,15 +31,6 @@ export default defineComponent({
     CodeBlock,
   },
   setup() {
-    // const listDockerCommands = ref<string[]>([
-    //   'docker run -d -p 80:3060 docker/getting-started',
-    //   'docker containers ls',
-    //   'docker build -t <container-tag-name> .',
-    //   'docker ps',
-    //   'docker stop <container-id>',
-    //   'docker rm <container-id>',
-    //   'docker exec <container-id>'
-    // ])
     const dockerCommandslist = reactive<DockerCommand[]>([
       {
         name: 'Docker Run',
@@ -51,21 +42,45 @@ export default defineComponent({
         ],
       },
       {
-        name: 'Docker Run',
-        command: 'docker run -d -p 80:3060 docker/getting-started',
+        name: 'Docker list out containers',
+        command: 'docker containers ls',
         details: [
-          '-d is a flag that allow the container running in the background',
-          '-p 80:3060 is a arugments that allow user to map a host port 80 to a container port 3060',
-          'docker/getting-started is a image that we are running',
+          'list out all of the containers that is currecntly running within the host',
         ],
       },
       {
-        name: 'Docker Run',
-        command: 'docker run -d -p 80:3060 docker/getting-started',
+        name: 'Docker list out images',
+        command: 'docker images ls',
+        details: ['list out all of the image within your docker'],
+      },
+      {
+        name: 'Docker build a DockerFile into a DockerImage',
+        command: 'docker build -t <container-tag-name> .',
         details: [
-          '-d is a flag that allow the container running in the background',
-          '-p 80:3060 is a arugments that allow user to map a host port 80 to a container port 3060',
-          'docker/getting-started is a image that we are running',
+          '-t is a flag that allow user to tag a container with the name they wanted to be',
+          'the . at the end of the command will tell docker to look for a Dockerfile within the currecnt directory',
+        ],
+      },
+      {
+        name: 'Docker list process',
+        command: 'docker ps',
+        details: ['list out all the running containers within the host'],
+      },
+      {
+        name: 'Stop Docker container',
+        command: 'docker stop <container-id>',
+        details: ['stop a container using their id'],
+      },
+      {
+        name: 'Remove Docker stopped container',
+        command: 'docker rm <container-id>',
+        details: ['remove stopped container using their id'],
+      },
+      {
+        name: 'SSH into the running container instance',
+        command: 'docker exec <container-id>',
+        details: [
+          'To dive into the container via cli then you can run some basic linux commands',
         ],
       },
     ]);
