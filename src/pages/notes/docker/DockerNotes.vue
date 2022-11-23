b0a58faecdf6e9fded6c4ed2f839b02b1b63f9ae
wilson nie <wilson.nie13@gmail.com>
2022-11-23 12:14:36 -0800

    q tree added when click on a node it will jump to a elment by ids

--- src/pages/notes/docker/DockerNotes.vue
+++ src/pages/notes/docker/DockerNotes.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page class="col justify-evenly note-content">
     <div
-      v-for="dockerCommand in dockerCommandslist"
+      v-for="(dockerCommand, index) in dockerCommandslist"
       :key="dockerCommand.command"
     >
-      <h5 class="header-space">{{ dockerCommand.name }}</h5>
+      {{ index }}
+      <h5 class="header-space" :id="`header-${index.toString()}`">
+        {{ dockerCommand.name }}
+      </h5>
       <q-separator />
       <code-block :codeString="dockerCommand.command" />
       <ol>
@@ -14,20 +17,16 @@
       </ol>
     </div>
     <div class="q-pa-md q-gutter-sm">
-      <q-tree
-        :nodes="lazy"
-        default-expand-all
-        node-key="label"
-        @lazy-load="onLazyLoad"
-      />
+      <q-tree :nodes="simple" node-key="label" />
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue';
+import { defineComponent, onMounted, reactive, ref } from 'vue';
 import CodeBlock from 'src/components/CodeBlock.vue';
 import { DockerCommand } from 'src/components/Schemas/ComponentSchema';
+import { dom } from 'quasar';
 
 export default defineComponent({
   name: 'DockerNotes',
@@ -90,56 +89,65 @@ export default defineComponent({
         ],
       },
     ]);
-    const nodes = [
+    const simple = [
       {
-        label: 'Node 1',
+        label: 'Docker Basic',
+        icon: 'fa-brands fa-docker',
         children: [
-          { label: 'Node 1.1', lazy: true },
-          { label: 'Node 1.2', lazy: true },
-        ],
-      },
-      {
-        label: 'Node 2',
-        lazy: true,
-      },
-      {
-        label: 'Lazy load empty',
-        lazy: true,
-      },
-      {
-        label: 'Node is not expandable',
-        expandable: false,
-        children: [{ label: 'Some node' }],
-      },
-    ];
-
-    const lazy = ref(nodes);
-
-    const onLazyLoad = ({ node, key, done, fail }) => {
-      // call fail() if any error occurs
-
-      setTimeout(() => {
-        // simulate loading and setting an empty node
-        if (key.indexOf('Lazy load empty') > -1) {
-          done([]);
-          return;
-        }
-
-        const label = node.label;
-        done([
-          { label: `${label}.1` },
-          { label: `${label}.2`, lazy: true },
           {
-            label: `${label}.3`,
+            label: 'Good food (with icon)',
+            icon: 'restaurant_menu',
             children: [
-              { label: `${label}.3.1`, lazy: true },
-              { label: `${label}.3.2`, lazy: true },
+              { label: 'Quality ingredients' },
+              { label: 'Good recipe' },
+            ],
+            handler: function (node) {
+              console.log(node);
+              console.log('test');
+              document
+                .getElementById('header-0')
+                .scrollIntoView({ behavior: 'smooth' });
+            },
+          },
+          {
+            label: 'Good service (disabled node with icon)',
+            icon: 'room_service',
+            disabled: true,
+            children: [
+              { label: 'Prompt attention' },
+              { label: 'Professional waiter' },
             ],
           },
-        ]);
-      }, 100);
+          {
+            label: 'Pleasant surroundings (with icon)',
+            icon: 'photo',
+            children: [
+              {
+                label: 'Happy atmosphere (with image)',
+                img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png',
+              },
+              { label: 'Good table presentation' },
+              { label: 'Pleasing decor' },
+            ],
+          },
+        ],
+      },
+    ];
+    const test = (node) => {
+      // console.log('node being pressed');
+      // console.log(node);
+      // document.getElementById('header-0').scrollIntoView();
     };
-    return { dockerCommandslist, onLazyLoad, nodes, lazy };
+    onMounted(() => {
+      console.log('mounted');
+      console.log(document.getElementById('header-0'));
+    });
+    // const { ready } = dom;
+    // ready(() => {
+    //   console.log(dom);
+    //   console.log(document.getElementById('header-0'));
+    // });
+    return { dockerCommandslist, simple, test };
   },
 });
 </script>
