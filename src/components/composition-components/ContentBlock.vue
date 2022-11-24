c444eb781ab2960f79d5af364e21efecf35c9108
wilson nie <wilson.nie13@gmail.com>
2022-11-24 08:54:01 -0800

    more refactoring

--- src/components/composition-components/ContentBlock.vue
+++ src/components/composition-components/ContentBlock.vue
@@ -1,34 +1,53 @@
 <template>
-  <div> content block </div>
+  <div>
+    <h5 class="content-header">{{ contentName }}</h5>
+    <q-separator color="black" />
+    <code-block v-if="commandLine" :code-string="commandLine"></code-block>
+    <ol v-if="contentDetails">
+        <li v-for="detail in contentDetails" :key="detail">
+          {{ detail }}
+        </li>
+    </ol>
+
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
-
+import CodeBlock from '../single_components/CodeBlock.vue';
 export default defineComponent({
   name: 'ContentBlock',
+  components: {
+    CodeBlock,
+  },
   props: {
     contentName: {
       type: String,
-      required: true
+      required: true,
     },
     contentDescription: {
       type: String,
-      required: true
+      required: true,
     },
-
     commandLine: {
       type: String,
-      required: false
+      required: false,
     },
     contentDetails: {
-      type: Array as PropType<Array<number>>,
-      required: false
+      type: Array as PropType<Array<string>>,
+      required: false,
     },
     externalLink: {
       type: String,
-      required: false
-    }
-  }
+      required: false,
+    },
+  },
 });
 </script>
+
+<style scoped>
+.content-header {
+  background-color: white;
+  margin-bottom: 0;
+}
+</style>
--- src/pages/notes/github/GithubNotes.vue
+++ src/pages/notes/github/GithubNotes.vue
@@ -1,13 +1,22 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div>Basic Github Notes</div>
+    <content-block-vue
+      content-name="test"
+      content-description="test"
+      command-line="test"
+      :content-details="['details 1', 'details 2']"
+    ></content-block-vue>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ContentBlockVue from 'src/components/composition-components/ContentBlock.vue';
 
 export default defineComponent({
   name: 'GithubNotes',
+  components: {
+    ContentBlockVue,
+  },
 });
 </script>
