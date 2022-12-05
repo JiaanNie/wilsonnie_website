81d301bf9ac9a7c1df85ce09a401105bc985dc2b
wilson nie <wilson.nie13@gmail.com>
2022-12-05 07:37:30 -0800

    setting up a stepper

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,7 @@ import DockerNotes from 'src/pages/notes/docker/DockerNotes.vue';
 import GridNotes from 'src/pages/notes/css-notes/GridNotes.vue';
 import RickAndRoll from 'src/pages/notes/others/RickAndRoll.vue';
 import BasicDeployingQuasarApp from 'src/pages/notes/others/BasicDeployingQuasarApp.vue';
+import CoverLetterGenerator from 'src/pages/cover-letter/CoverLetterGenerator.vue';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -31,7 +32,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/cover-letter-generator',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', name: 'CoverLetterGenerator', component: CoverLetterGenerator }],
   },
 
   // Always leave this as last one,
