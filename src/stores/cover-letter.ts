33d81b05422ff40cc7d754ac88611ed1edc7f13b
wilson nie <wilson.nie13@gmail.com>
2022-12-08 08:41:45 -0800

    settting up pinia with coverletter

--- src/stores/cover-letter.ts
+++ src/stores/cover-letter.ts
@@ -1,29 +1,95 @@
-import { stat } from 'fs';
 import { defineStore } from 'pinia';
-import { Sender } from './schemas/StateSchema';
+import {
+  Sender,
+  Receiver,
+  Position,
+  Company,
+  Introduction,
+  Behavior,
+  Conclusion,
+  Experience,
+} from './schemas/StateSchema';
 
 export const useCounterStore = defineStore('coverLetter', {
   state: () => ({
-    sender: {
+    reciver: {
+      name: 'reciver',
       value: 'Human Resource',
-      defaultValue: 'Human Resource'
-    } as Sender
-
+      defaultValue: 'Human Resource',
+      inputType: 'input',
+      icon: 'setting',
+      description: '',
+    } as Receiver,
+    position: {
+      name: 'position',
+      value: '',
+      defaultValue: '',
+      inputType: 'input',
+      icon: 'setting',
+      description: '',
+    } as Position,
+    company: {
+      name: 'company',
+      value: '',
+      defaultValue: '',
+      inputType: 'input',
+      icon: 'setting',
+      description: '',
+    } as Company,
+    introduction: {
+      name: 'introduction',
+      value: '',
+      defaultValue: '',
+      inputType: 'editor',
+      icon: 'setting',
+      description: '',
+    } as Introduction,
+    experience: {
+      name: 'experience',
+      value: '',
+      defaultValue: '',
+      inputType: 'editor',
+      icon: 'setting',
+      description: '',
+    } as Experience,
+    behavior: {
+      name: 'behavior',
+      value: '',
+      defaultValue: '',
+      inputType: 'editor',
+      icon: 'setting',
+      description: '',
+    } as Behavior,
+    conclusion: {
+      name: 'conclusion',
+      value: '',
+      defaultValue: '',
+      inputType: 'editor',
+      icon: 'setting',
+      description: '',
+    } as Conclusion,
+    sender: {
+      name: 'sender',
+      value: 'Jiaan (Wilson) Nie',
+      defaultValue: 'Jiaan (Wilson) Nie',
+      inputType: 'input',
+      icon: 'setting',
+      description: '',
+    } as Sender,
   }),
 
   getters: {
-    doubleCount (state) {
+    doubleCount(state) {
       return state.counter * 2;
     },
-    resetToDefault (state) {
-      return state.sender.value = state.sender.defaultValue
-    }
-
   },
 
   actions: {
-    increment () {
+    increment() {
       this.counter++;
-    }
-  }
+    },
+    resetToDefault(state) {
+      return (state.sender.value = state.sender.defaultValue);
+    },
+  },
 });
--- src/stores/schemas/StateSchema.ts
+++ src/stores/schemas/StateSchema.ts
@@ -1,4 +1,17 @@
-export interface Sender {
-  defaultValue?: string
-  value: string
+export interface CoverLetterComponent {
+  defaultValue: string;
+  value: string;
+  inputType: string;
+  icon: string;
+  description: string;
+  name: string;
 }
+
+export type Receiver = CoverLetterComponent;
+export type Position = CoverLetterComponent;
+export type Company = CoverLetterComponent;
+export type Introduction = CoverLetterComponent;
+export type Experience = CoverLetterComponent;
+export type Behavior = CoverLetterComponent;
+export type Conclusion = CoverLetterComponent;
+export type Sender = CoverLetterComponent;
