3f7355c7f290f419df45c6a47a2d83f626d2f8ed
wilson nie <wilson.nie13@gmail.com>
2022-12-10 19:36:38 -0800

    now the reumae page get access to the resume data in the store

--- src/pages/resume/Resume.vue
+++ src/pages/resume/Resume.vue
@@ -6,8 +6,31 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useResumeStore } from 'src/stores/resume';
 
 export default defineComponent({
   name: 'MyResume',
+  setup() {
+    const store = useResumeStore();
+    const experiences = store.getExperiences;
+    const skills = store.getSkills;
+    const contact = store.getContact;
+    const educations = store.getEducations;
+    const location = store.getLocation;
+    const socials = store.getSoicals;
+    const projects = store.getProjects;
+    const aboutMe = store.getAboutMe;
+
+    return {
+      skills,
+      experiences,
+      contact,
+      educations,
+      location,
+      socials,
+      projects,
+      aboutMe,
+    };
+  },
 });
 </script>
