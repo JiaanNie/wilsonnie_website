e99af2cda1e759597716848fdc792b860645a347
wilson nie <wilson.nie13@gmail.com>
2022-12-12 14:02:20 -0800

    new card component added

--- src/pages/notes/aws/AWSNotes.vue
+++ src/pages/notes/aws/AWSNotes.vue
@@ -1,23 +1,26 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <q-card class="my-card">
+      <div class="q-pa-md">
+        <q-avatar
+          color="pink"
+          text-color="white"
+          icon="work"
+          style="top: 0; left: 22px; transform: translateY(-90%)"
+        >
+        </q-avatar>
+      </div>
+
       <q-card-section>
-        <q-icon
-          fab
-          color="primary"
-          name="work"
-          class="absolute"
-          size="xl"
-          style="top: 0; left: 12px; transform: translateY(-50%)"
-        ></q-icon>
-
-        <div class="row no-wrap items-center">sdfdsfafds</div>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none wrap">
-        sdfsdfdsfffffffffffffffffffffffffffffffffffffffsdfffffffffffffffdsffffffffffffffffffffffffffffff
-      </q-card-section>
+      <q-separator inset />
 
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
       <q-separator />
 
       <q-card-actions>
@@ -33,5 +36,21 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'AWSbNotes',
+  setup() {
+    return {
+      lorem:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    };
+  },
 });
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+
+.q-pa-md
+  width: 0
+  height: 0
+</style>
