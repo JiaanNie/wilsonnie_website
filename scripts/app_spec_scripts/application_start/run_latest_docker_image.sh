58fe2be099131df35b7760ab8a1cdd5ecbca9a7b
wilson nie <wilson.nie13@gmail.com>
2023-02-03 14:10:36 -0800

    attemp to fix nginx erros

--- scripts/app_spec_scripts/application_start/run_latest_docker_image.sh
+++ scripts/app_spec_scripts/application_start/run_latest_docker_image.sh
@@ -4,8 +4,7 @@ echo "Listing out images"
 sudo docker images
 
 export IMAGE_NAME="$(cat /home/ubuntu/app/temp.txt)"
-image_id=sudo docker images -q $IMAGE_NAME
-
+export image_id=sudo docker images -q $IMAGE_NAME
 echo current image id is $image_id
 echo "Run the new deploy image"
 # sudo docker run -d -p 80:80 "$(cat /home/ubuntu/app/temp.txt)"
@@ -15,7 +14,7 @@ echo "Run the new deploy image"
 sudo docker run --detach --name nginx-proxy --publish 80:80 --publish 443:443 --volume certs:/etc/nginx/certs --volume vhost:/etc/nginx/vhost.d --volume html:/usr/share/nginx/html --volume /var/run/docker.sock:/tmp/docker.sock:ro nginxproxy/nginx-proxy
 sudo docker run --detach --name nginx-proxy-acme --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro --volume acme:/etc/acme.sh --env "DEFAULT_EMAIL=wilson.nie13@gmail.com" nginxproxy/acme-companion
 
-sudo docker run --detach --name personal-site --env "VIRTUAL_HOST=wilsonnie.ca" --env "LETSENCRYPT_HOST=wilsonnie.ca" $image_id
+sudo docker run --detach --name personal-site --env "VIRTUAL_HOST=wilsonnie.ca" --env "LETSENCRYPT_HOST=wilsonnie.ca" 8bcc9df00fb4
 
 #remove the temp file
 sudo rm /home/ubuntu/app/temp.txt
